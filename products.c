8947872d7f56c8021c7df98e8595f1da34febf3f
pritikmshaw <pritikmshaw18@gmail.com>
2020-08-13 22:12:03 +0530

    products of two numbers

---
+++ products.c
@@ -0,0 +1,11 @@
+// products of two numbers
+#include<stdio.h>
+void main()
+{
+	int a ,b , products;
+	printf(" enter a and b : ");
+	scanf(" %d %d", &a,&b);
+	products = a*b ;
+	printf(" products = %d ", products);
+	
+}
