e16b10d87ecf8809e50c7c29568809989cf4258d
pritikmshaw <pritikmshaw18@gmail.com>
2020-08-18 18:28:39 +0530

    ascii value of any character

---
+++ ascii_value.c
@@ -0,0 +1,10 @@
+// program to find the ascii value of a character
+#include<stdio.h>
+int main()
+{
+	char ch;
+	printf("enter the word : ");
+	scanf("%c",&ch);
+	printf("the ascii value of %c is %d",ch,ch);
+	return(0);
+}
